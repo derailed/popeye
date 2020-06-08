889725e71f6f035712d6f649de3858ed9f419ae9
derailed <fernand.galiana@gmail.com>
2020-06-08 13:38:35 -0600

    cleaning up

--- internal/scrub/cache_apps.go
+++ internal/scrub/cache_apps.go
@@ -8,7 +8,6 @@ import (
 	"github.com/derailed/popeye/internal/cache"
 	"github.com/derailed/popeye/internal/client"
 	"github.com/derailed/popeye/internal/dag"
-	"github.com/rs/zerolog/log"
 )
 
 type apps struct {
@@ -92,11 +91,9 @@ func (a *apps) context() (context.Context, context.CancelFunc) {
 	ctx := context.WithValue(context.Background(), internal.KeyFactory, a.factory)
 	ctx = context.WithValue(ctx, internal.KeyConfig, a.config)
 	if a.config.Flags.ActiveNamespace != nil {
-		log.Debug().Msgf("CONTEXT-OVER -- %q", *a.config.Flags.ActiveNamespace)
 		ctx = context.WithValue(ctx, internal.KeyNamespace, *a.config.Flags.ActiveNamespace)
 	} else {
 		ns, err := a.factory.Client().Config().CurrentNamespaceName()
-		log.Debug().Msgf("CONTEXT-CLIENT -- %q::%v", ns, err)
 		if err != nil {
 			ns = client.AllNamespaces
 		}
