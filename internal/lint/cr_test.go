22d02b93de0d5d7a1f12dc716a39b3d7c62f8e6a
Jordi Prats <jordi.prats@gmail.com>
2024-04-26 18:45:58 +0200

    ClusterRole: add aggregationRule support

--- internal/lint/cr_test.go
+++ internal/lint/cr_test.go
@@ -43,3 +43,22 @@ func TestCRLint(t *testing.T) {
 	assert.Equal(t, `[POP-400] Used? Unable to locate resource reference`, ii[0].Message)
 	assert.Equal(t, rules.InfoLevel, ii[0].Level)
 }
+
+func TestCRLintAggregations(t *testing.T) {
+	dba, err := test.NewTestDB()
+	assert.NoError(t, err)
+	l := db.NewLoader(dba)
+
+	ctx := test.MakeCtx(t)
+	assert.NoError(t, test.LoadDB[*rbacv1.ClusterRole](ctx, l.DB, "auth/cr/2.yaml", internal.Glossary[internal.CR]))
+
+	cr := NewClusterRole(test.MakeCollector(t), dba)
+	assert.Nil(t, cr.Lint(test.MakeContext("rbac.authorization.k8s.io/v1/clusterroles", "clusterroles")))
+	assert.Equal(t, 2, len(cr.Outcome()))
+
+	ii := cr.Outcome()["cr4"]
+	assert.Equal(t, 1, len(ii))
+
+	ii = cr.Outcome()["cr5"]
+	assert.Equal(t, 0, len(ii))
+}
